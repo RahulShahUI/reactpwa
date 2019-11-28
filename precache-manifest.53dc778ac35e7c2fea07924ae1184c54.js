fe429666856161a445c7bcbcf33bd518cb3d201d
Rahul Shah <rahul.shah@credencys.com>
2019-11-28 17:30:46 +0530

    reactpwa added after github fixes

---
+++ precache-manifest.53dc778ac35e7c2fea07924ae1184c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998e87b1d8c4199d593047a786c4e689",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7425837bf3ee91f0adc0",
+    "url": "/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "346798aec4a8beec8b72",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7425837bf3ee91f0adc0",
+    "url": "/static/js/2.ec0bc7e1.chunk.js"
+  },
+  {
+    "revision": "346798aec4a8beec8b72",
+    "url": "/static/js/main.9ea41547.chunk.js"
+  },
+  {
+    "revision": "1880bfb597f93348c730",
+    "url": "/static/js/runtime-main.e1b57d57.js"
+  }
+]);
