5229c1ce6b4ee313b4884934d4fedc7c45a55125
Rahul Shah <rahul.shah@credencys.com>
2019-11-29 10:45:07 +0530

    test vuild

---
+++ precache-manifest.1197d23fff083c679a57c59f169ad18d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329a74939d62e4dd30d0fadc718506bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19deef77bff89e1f9633",
+    "url": "./static/css/2.5c417c01.chunk.css"
+  },
+  {
+    "revision": "e44daa6195436d1ae933",
+    "url": "./static/css/main.94d028df.chunk.css"
+  },
+  {
+    "revision": "19deef77bff89e1f9633",
+    "url": "./static/js/2.1af29ea1.chunk.js"
+  },
+  {
+    "revision": "e44daa6195436d1ae933",
+    "url": "./static/js/main.24bdefcc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "2407b512f6f2eec61cd2f3136242a025",
+    "url": "./static/media/Ring-Preloader.2407b512.gif"
+  },
+  {
+    "revision": "d8c56e7f048f878ee0e0e7a8c9caea0a",
+    "url": "./static/media/dependancy-icon.d8c56e7f.svg"
+  },
+  {
+    "revision": "8b4712863901de9af93d0e9dcdd7255d",
+    "url": "./static/media/fb-icn.8b471286.svg"
+  },
+  {
+    "revision": "3e3d084fd2fef7a4c85451d8b4627631",
+    "url": "./static/media/filter.3e3d084f.svg"
+  },
+  {
+    "revision": "27ce75282d6765b00c421574a3eb5e86",
+    "url": "./static/media/folder.27ce7528.svg"
+  },
+  {
+    "revision": "78d4bf93210153e3635fbe6c89a67d43",
+    "url": "./static/media/login-banner.78d4bf93.jpg"
+  },
+  {
+    "revision": "0bf587a345dbd6b31757171f6112863f",
+    "url": "./static/media/logo-admin.0bf587a3.png"
+  },
+  {
+    "revision": "521485aa2136105739b6a2c44b081618",
+    "url": "./static/media/logo.521485aa.png"
+  },
+  {
+    "revision": "288ee24e7316bec5981e281217120ad0",
+    "url": "./static/media/no-contact.288ee24e.jpg"
+  },
+  {
+    "revision": "5014e0bf85d6575b90e7a5358df81c40",
+    "url": "./static/media/no-record.5014e0bf.png"
+  },
+  {
+    "revision": "2f924086cb7a604f61714adaf3fd7321",
+    "url": "./static/media/no-template.2f924086.jpg"
+  },
+  {
+    "revision": "974f0e6307f077f82a316a485e0283e6",
+    "url": "./static/media/no-transaction.974f0e63.png"
+  },
+  {
+    "revision": "ff71372d153e50f36013168082e78b66",
+    "url": "./static/media/pdf-test.ff71372d.pdf"
+  },
+  {
+    "revision": "a902e70cff93f451da6dee928b2a0d01",
+    "url": "./static/media/property-placeholder.a902e70c.jpg"
+  },
+  {
+    "revision": "ec9031fd54eb0842c9c543d92a7d226e",
+    "url": "./static/media/transaction.ec9031fd.jpg"
+  },
+  {
+    "revision": "1e8a5d316aea190f8659083c7912320b",
+    "url": "./static/media/tw-icn.1e8a5d31.svg"
+  },
+  {
+    "revision": "51a573f4be168114f7598b6dd970a0a7",
+    "url": "./static/media/user.51a573f4.jpeg"
+  }
+]);
