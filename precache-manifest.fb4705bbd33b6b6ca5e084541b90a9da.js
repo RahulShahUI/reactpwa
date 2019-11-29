af4c5a50f82397a25adfad3f4b34ce2506275ea3
Rahul Shah <rahul.shah@credencys.com>
2019-11-29 11:14:04 +0530

    new build for base project

---
+++ precache-manifest.fb4705bbd33b6b6ca5e084541b90a9da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c19dfb694dbfa3cb37a74a3f8d727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46be09da2c5cfe77ba0f",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4d9b2f9112350d9d15a8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "46be09da2c5cfe77ba0f",
+    "url": "/static/js/2.93238a67.chunk.js"
+  },
+  {
+    "revision": "4d9b2f9112350d9d15a8",
+    "url": "/static/js/main.72ceeea5.chunk.js"
+  },
+  {
+    "revision": "484f09e3befcacb1ba00",
+    "url": "/static/js/runtime-main.b0706642.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
